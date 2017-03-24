fab88981f001ac96ddbef820b9f59c4735487771
Yassine Rachid <am45880@etsmtl.net>
2017-03-24 16:56:58 -0500

    Commit before to secure the API

---
+++ src/main/java/com/findr/controllers/DataController.java
@@ -0,0 +1,87 @@
+package com.findr.controllers;
+
+import com.findr.model.User;
+import com.findr.repositories.UserRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Yassine on 2017-03-24.
+ */
+@RestController
+@CrossOrigin
+@RequestMapping("/data")
+public class DataController {
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @RequestMapping("delete")
+    public String deleteUsers(){
+        userRepo.deleteAll();
+        return "all users has been deleted";
+    }
+
+
+    @RequestMapping("/init")
+    public String creatUsers() {
+
+        List<User> users = new ArrayList<User>();
+
+        users.add(
+                new User("Caroline", "leblanc", "F", "M", "caro@gmail.com", "514 236 9823",
+                        31, "I am beautiful girl looking for nice men.", "secret123")
+        );
+
+        users.add(
+                new User("Christina", "Blanchette", "F", "M", "christi@gmail.com", "438 236 0382",
+                        38, "I am beautiful girl looking for nice men.", "secret123")
+        );
+
+        users.add(
+                new User("Sabrina", "Ouelette", "F", "M", "wawa.sab@gmail.com", "450 324 3418",
+                        27, "I am beautiful girl looking for nice men.", "secret123")
+        );
+
+        users.add(
+                new User("Yell", "Fantas", "F", "M", "yello@gmail.com", "450 324 3418",
+                        29, "I am beautiful girl looking for nice men.", "secret123")
+        );
+
+        users.add(
+                new User("George", "Tremblay", "M", "F", "George@gmail.com", "514 233 0983",
+                        23, "Looking for nice women.", "secret123"
+                ));
+
+        users.add(
+                new User("Kreg", "Jiga", "M", "F", "Kregy11@gmail.com", "514 233 0233",
+                        25, "Looking for nice women.", "secret123"
+                ));
+        users.add(
+                new User("Zac", "Lacroche", "M", "F", "lacromi@gmail.com", "514 223 3243",
+                        32, "Looking for nice women.", "secret123"
+                ));
+        users.add(
+                new User("Maitre", "Yoda", "M", "F", "yoda@gmail.com", "XER 2K3 3243",
+                        28, "Looking for nice women.", "secret123"
+                ));
+        users.add(
+                new User("Jeff", "Gay", "M", "F", "lgbt432@gmail.com", "450 324 2341",
+                        26, "Looking for nice men", "secret123"
+                ));
+
+        users.add(
+                new User("Jerry", "Gayy", "M", "F", "pro-choice@gmail.com", "514 234 5423",
+                        26, "Looking for nice men", "secret123"
+                ));
+
+        userRepo.save(users);
+
+        return "10 users has been created";
+    }
+}
--- src/main/java/com/findr/controllers/UsersAPI.java
+++ src/main/java/com/findr/controllers/UsersAPI.java
@@ -1,7 +1,6 @@
 package com.findr.controllers;
 import com.findr.model.User;
 import com.findr.repositories.UserRepo;
-import com.findr.util.UsersGenerator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,6 +13,7 @@ import java.util.List;
  * Created by Yassine on 2017-03-20.
  */
 @RestController
+@CrossOrigin
 @RequestMapping("/")
 public class UsersAPI {
 
@@ -21,41 +21,11 @@ public class UsersAPI {
     private UserRepo userRepo;
 
     @RequestMapping
-    @CrossOrigin(origins = "*")
     public List<User> getUser(){
 
-        User utilisateur1 =
-                new User("Caroline","leblanc","F","M","caro@gmail.com","514 236 9823",
-                        28,"I am beautiful girl looking for nice men.","secret123");
-        User utilisateur2 =
-                new User("George","Tremblay","M","F","George@gmail.com","514 233 0983",
-                        23,"Looking for nice women.","secret123");
-        User utilisateur3 =
-                new User("Kreg","Jiga","M","F","Kregy11@gmail.com","514 233 0233",
-                        25,"Looking for nice women.","secret123");
-        User utilisateur4 =
-                new User("Zac","Lacroche","M","F","lacromi@gmail.com","514 223 3243",
-                        25,"Looking for nice women.","secret123");
-        User utilisateur5 =
-                new User("Maitre","Yoda","M","F","yoda@gmail.com","XER 2K3 3243",
-                        25,"Looking for nice women.","secret123");
-
-        List<User> users = new ArrayList<User>();
-
-        users.add(utilisateur1);
-        users.add(utilisateur2);
-        users.add(utilisateur3);
-        users.add(utilisateur4);
-        users.add(utilisateur5);
+        List<User> users = userRepo.findAll();
 
         return users;
     }
 
-    @RequestMapping("/create")
-    @CrossOrigin(origins = "*")
-    public String creatUsers(){
-        UsersGenerator usersGenerator = new UsersGenerator();
-        usersGenerator.createFiveUser();
-        return "The users have been created.";
-    }
 }
