6bfdb6fc969c2caae2f48c27b932f8fd27e1d681
Yassine Rachid <am45880@etsmtl.net>
2017-03-29 18:18:21 -0500

    Login

--- src/main/java/com/findr/configuration/UserAuthService.java
+++ src/main/java/com/findr/configuration/UserAuthService.java
@@ -1,36 +1,36 @@
-package com.findr.configuration;
-
-
-import com.findr.repositories.UserRepo;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.AuthorityUtils;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Component;
-
-
-
-@Component
-public class UserAuthService implements UserDetailsService {
-
-
-    @Autowired
-    private UserRepo userRepo;
-
-    @Autowired
-    public UserAuthService(UserRepo userRepo) {
-        this.userRepo = userRepo;
-    }
-
-    @Override
-    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
-
-        com.findr.model.User user = this.userRepo.findByEmail(email);
-
-        return new User(user.getEmail(),user.getPassword(),AuthorityUtils.createAuthorityList("ROLE_USER"));
-
-    }
-
-}
+//package com.findr.configuration;
+//
+//
+//import com.findr.repositories.UserRepo;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.authority.AuthorityUtils;
+//import org.springframework.security.core.userdetails.User;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Component;
+//
+//
+//
+//@Component
+//public class UserAuthService implements UserDetailsService {
+//
+//
+//    @Autowired
+//    private UserRepo userRepo;
+//
+//    @Autowired
+//    public UserAuthService(UserRepo userRepo) {
+//        this.userRepo = userRepo;
+//    }
+//
+//    @Override
+//    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+//
+//        com.findr.model.User user = this.userRepo.findByEmail(email);
+//
+//        return new User(user.getEmail(),user.getPassword(),AuthorityUtils.createAuthorityList("ROLE_USER"));
+//
+//    }
+//
+//}
